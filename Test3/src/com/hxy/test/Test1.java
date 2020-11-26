948a17f014cd9c5783e35d21c723b96a8bf64caf
huhuxx <1647524030@qq.com>
2020-11-26 10:44:36 +0800

    123

---
+++ Test3/src/com/hxy/test/Test1.java
@@ -0,0 +1,12 @@
+package com.hxy.test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void a() {
+		assertEquals(2, 2);
+	}
+}
