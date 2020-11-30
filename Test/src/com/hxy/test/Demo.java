0c7945e6490ba9809e24ce4a5a31ad4e94f83ee8
huhuxx <1647524030@qq.com>
2020-11-30 12:02:33 +0800

    15

--- Test/src/com/hxy/test/Demo.java
+++ Test/src/com/hxy/test/Demo.java
@@ -1,5 +1,7 @@
 package com.hxy.test;
 
+
+
 import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
@@ -7,6 +9,7 @@ import org.testng.annotations.Test;
 public class Demo {
   @Test
   public void f() {
+	  System.out.println("1");
 	  assertEquals("1", "1");
   }
 }
